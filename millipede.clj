3d052439fbf2356366c6b6087f25366449b716e1
Manfred Touron <m@42.am>
2015-07-27 13:02:39 +0200

    Refactore clojure code

--- millipede.clj
+++ millipede.clj
@@ -1,8 +1,7 @@
-(defn padding-str [n]
-  (str (apply str (repeat (min (mod n 8) (- 8 (mod n 8))) " "))))
+#!/usr/bin/env clj
 
 (defn print-body [n]
-  (println (format "%s╚═(███)═╝" (padding-str n))))
+  (println (format "%s╚═(███)═╝" (apply str (repeat (min (mod n 8) (- 8 (mod n 8))) " ")))))
 
 (defn millipede [n]
   (println "  ╚⊙ ⊙╝")
